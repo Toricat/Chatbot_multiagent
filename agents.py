f6190cbe2127c410555093b6f991b85d7d4d80e8
HaMinhQuan <99237494+CatKingio@users.noreply.github.com>
2023-12-28 12:55:02 +0700

    Update RAG modul

--- agents.py
+++ agents.py
@@ -42,7 +42,7 @@ class assistant_chat:
 
 
 class application_group:     
-
+    
     def __init__(self, llm_config):
         self.llm_config = llm_config
         self.init_agents()
@@ -50,59 +50,98 @@ class application_group:
     def init_agents(self):
         self.user_proxy = MyConversableAgent(
         name="Admin",
-        is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("exit"),
+        max_consecutive_auto_reply=20,
+        is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
         system_message="""A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin. 
-        
+        Don reply thank you, only reply TERMINATE if the task has been solved at full satisfaction.
+        Otherwise, reply CONTINUE, or the reason why the task is not solved yet.
         """,
-        code_execution_config=False,
+        #Only say APPROVED in most cases, and say exit when nothing to be done further. Do not say others.
+        code_execution_config={"work_dir": "web"},
         #default_auto_reply="Approved", 
         human_input_mode="ALWAYS",
-        llm_config= self.llm_config,
+        llm_config=self.llm_config,
         )
 
-        self.engineer = autogen.AssistantAgent(
-            name="Engineer",
+        self.coder = autogen.AssistantAgent(
+            name="Coder",
             human_input_mode="NEVER",
-            code_execution_config={"last_n_messages": 3, "work_dir": "code","use_docker":True},
-            llm_config= self.llm_config,
+            llm_config=self.llm_config,
             system_message='''Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
-        Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
-        If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
-        ''',
+            Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
+            If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
+        
+            ''',
         )
         self.scientist = autogen.AssistantAgent(
             name="Scientist",
             human_input_mode="NEVER",
-            llm_config= self.llm_config,
-            system_message="""Scientist. You follow an approved plan. You are able to categorize papers after seeing their abstracts printed. You don't write code."""
+            llm_config=self.llm_config,
+            is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+            system_message="""Scientist. You follow an approved plan. If have paper, you are able to categorize papers after seeing their abstracts printed. You don't write code.
+            Don reply thank you, only reply TERMINATE if the task has been solved at full satisfaction.
+            Otherwise, reply CONTINUE, or the reason why the task is not solved yet.
+            """
         )
         self.planner = autogen.AssistantAgent(
             name="Planner",
             human_input_mode="NEVER",
+            is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
             system_message='''Planner. Suggest a plan. Revise the plan based on feedback from admin and critic, until admin approval.
         The plan may involve an engineer who can write code and a scientist who doesn't write code.
         Explain the plan first. Be clear which step is performed by an engineer, and which step is performed by a scientist.
+        Don reply thank you, only reply TERMINATE if the task has been solved at full satisfaction.
+        Otherwise, reply CONTINUE, or the reason why the task is not solved yet.
         ''',
-            llm_config= self.llm_config,
+            llm_config=self.llm_config,
         )
         self.executor = autogen.UserProxyAgent(
             name="Executor",
-            system_message="Executor. Execute the code written by the engineer and report the result.",
+            system_message="""Executor. Execute the code written by the engineer and report the result. Reply TERMINATE if the task has been solved at full satisfaction.
+            Otherwise, reply CONTINUE, or the reason why the task is not solved yet.""",
             human_input_mode="NEVER",
-            code_execution_config={"last_n_messages": 3, "work_dir": "paper"},
+            llm_config=self.llm_config,
+            code_execution_config={"work_dir": "web"},
         )
+        # self.critic = autogen.AssistantAgent(
+        #     name="Critic",
+        #     system_message="""Critic. Double check plan, claims, code from other agents and provide feedback. 
+        #     Check whether the plan includes adding verifiable info such as source URL. 
+        #     """,
+        #     llm_config=self.llm_config,
+        #     human_input_mode="NEVER",
+        # )
         self.critic = autogen.AssistantAgent(
             name="Critic",
-            system_message="""Critic. Double check plan, claims, code from other agents and provide feedback. 
-            Check whether the plan includes adding verifiable info such as source URL. 
+            system_message="""Critic. You are a helpful assistant highly skilled in evaluating the quality of a given visualization code by providing a score from 1 (bad) - 10 (good) while providing clear rationale. YOU MUST CONSIDER VISUALIZATION BEST PRACTICES for each evaluation. Specifically, you can carefully evaluate the code across the following dimensions
+            - bugs (bugs):  are there bugs, logic errors, syntax error or typos? Are there any reasons why the code may fail to compile? How should it be fixed? If ANY bug exists, the bug score MUST be less than 5.
+            - Data transformation (transformation): Is the data transformed appropriately for the visualization type? E.g., is the dataset appropriated filtered, aggregated, or grouped  if needed? If a date field is used, is the date field first converted to a date object etc?
+            - Goal compliance (compliance): how well the code meets the specified visualization goals?
+            - Visualization type (type): CONSIDERING BEST PRACTICES, is the visualization type appropriate for the data and intent? Is there a visualization type that would be more effective in conveying insights? If a different visualization type is more appropriate, the score MUST BE LESS THAN 5.
+            - Data encoding (encoding): Is the data encoded appropriately for the visualization type?
+            - aesthetics (aesthetics): Are the aesthetics of the visualization appropriate for the visualization type and the data?
+
+            YOU MUST PROVIDE A SCORE for each of the above dimensions.
+            {bugs: 0, transformation: 0, compliance: 0, type: 0, encoding: 0, aesthetics: 0}
+            Do not suggest code. 
+            Finally, based on the critique above, suggest a concrete list of actions that the coder should take to improve the code.
+            Don reply thank ypu, only reply TERMINATE if the task has been solved at full satisfaction.
+            Otherwise, reply CONTINUE, or the reason why the task is not solved yet.
             """,
+            is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
             llm_config= self.llm_config,
             human_input_mode="NEVER",
         )
+        # self.user_proxy.reset()
+        # self.coder.reset()
+        # self.scientist.reset()
+        # self.planner.reset()
+        # self.executor.reset()
+        # self.critic.reset()
         self.groupchat = autogen.GroupChat(
-            agents=[self.user_proxy, self.engineer, self.scientist, self.planner, self.executor, self.critic],
+            agents=[self.user_proxy, self.coder, self.scientist, self.planner, self.executor, self.critic],
             messages=[],
-            max_round=20
+            max_round=50,
         )
         self.manager = autogen.GroupChatManager(groupchat=self.groupchat, llm_config=self.llm_config)
 
@@ -116,9 +155,8 @@ class content_group:
     def init_agents(self):
         self.user_proxy = MyConversableAgent(
         name="Admin",
-        is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("exit"),
-        system_message="""A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin. You need to giving a task for all Employee.
-        """,
+        is_termination_msg=lambda x: x.get("content", "") and x.get("content", "").rstrip().endswith("TERMINATE"),
+        system_message="A human admin. You need to giving a task for all Employee.",
         code_execution_config=False,
         #default_auto_reply="Approved", 
         human_input_mode="ALWAYS",
@@ -128,18 +166,20 @@ class content_group:
             name="Content_creator",
             human_input_mode="NEVER",
             llm_config= self.llm_config,
-            system_message='''Content creator, you are an excellent content creator who can write about any topic requested by the admin. This includes a full range of diverse fields from content creation to writing. You will receive a content creation request from the admin and from there, please create content that corresponds with the request. After creating content, you should submit it to the Reviewer for evaluation and feedback. ''',
+            system_message="Content creator, you are an excellent content creator who can write about any topic requested by the admin. This includes a full range of diverse fields from content creation to writing. You will receive a content creation request from the admin and from there, please create content that corresponds with the request. After creating content, you should submit it to the Reviewer for evaluation and feedback. Only reply with TERMINATE once the entire project is completed.",
         )
         self.reviewer = autogen.AssistantAgent(
             name="reviewer",
             human_input_mode="NEVER",
             llm_config= self.llm_config,
-            system_message="""As a reviewer, your key task is to assess and critique content from our creators.
-             You'll evaluate its quality, relevance, and engagement, offering suggestions for improvement. Your responsibility to provide constructive feedback directly to the content creators. It's imperative that you consistently provide constructive feedback  that encourages innovation and enhancement."""
+            system_message="As a reviewer, your key task is to assess and critique content from our creators. You'll evaluate its quality, relevance, and engagement, offering suggestions for improvement. Your responsibility to provide constructive feedback directly to the content creators. It's imperative that you consistently provide constructive feedback  that encourages innovation and enhancement. Only reply with TERMINATE once the entire project is completed.",
         )
+        self.user_proxy.reset()
+        self.reviewer.reset()
+        self.reviewer.reset()
         self.groupchat = autogen.GroupChat(
             agents=[self.user_proxy, self.content_creator, self.reviewer],
             messages=[],
-            max_round=20
+            max_round=20,
         )
         self.manager = autogen.GroupChatManager(groupchat=self.groupchat, llm_config=self.llm_config)
--- app.py
+++ app.py
@@ -9,9 +9,12 @@ import asyncio
 from dotenv import load_dotenv
 from chat_state import ChatState
 from agents import *
+from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+import chromadb
 
-#panel serve app_test.py --show 
-
+#panel serve app.py --show 
+file_path = None
 
 load_dotenv()
 brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
@@ -22,7 +25,8 @@ config_list = autogen.config_list_from_json("OAI_CONFIG_LIST")
 mistral_7b_config={
     "config_list": config_list,
     "temperature":0, 
-    # "seed": 17
+    # "timeout": 600,
+     "cache_seed":42,
 }
 
 chat_state = ChatState.getInstance()
@@ -35,13 +39,14 @@ def print_messages( recipient, messages, sender, config):
         "assistant": "👩‍💼",
         "admin":"🧑‍💻",
         "user": "🧑",
-        "engineer": "👷‍♀️",
+        "coder": "👷‍♀️",
         "scientist": "👩‍🔬",
         "planner": "📅",
         "executor": "🔧",
         "critic": '🖋️',
         "content_creator":"🧑‍💻",
         "reviewer":"🧑‍💻",
+        "ragproxyagent":"🧑‍💻",
     }
     if name.lower() not in avatar:
         name = 'Secret man'
@@ -67,16 +72,21 @@ def register_agent_replies(agents):
 async def delayed_initiate_chat(agent, recipient, message):
     chat_state.initiate_chat_task_created = True
     await asyncio.sleep(2)
-    await agent.a_initiate_chat(recipient, message=message)
-    await chat_interface.send("press exit to close conversation", user="System", respond=False)
+    if  chat_state.current_mode == "file":
+        await agent.a_initiate_chat(recipient,problem=message)
+    else:
+        await agent.a_initiate_chat(recipient, message=message)
     
 pn.extension(design="material")
 
 async def callback(contents: str, user: str, instance: pn.chat.ChatInterface):
-
+    global file_path
     mode_1 = "create a snake game with python and pygame library"
     mode_2 = "write content"
+    mode_3 = "file"
     mode_normal = "other"
+
+    # Khai báo biến toàn cục
 
     if contents == "exit":
         chat_state.reset()
@@ -105,13 +115,27 @@ async def callback(contents: str, user: str, instance: pn.chat.ChatInterface):
             if not chat_state.initiate_chat_task_created:
                 print("Chat mode: content_group")
                 chat_agents =  content_group(mistral_7b_config)
-                register_agent_replies([chat_agents.user_proxy,chat_agents.user_proxy,
-                chat_agents.engineer,chat_agents.scientist,chat_agents.planner,chat_agents.executor ,chat_agents.critic])
+                register_agent_replies([chat_agents.user_proxy,
+                chat_agents.coder,chat_agents.scientist,chat_agents.planner,chat_agents.executor ,chat_agents.critic])
                 asyncio.create_task(delayed_initiate_chat(chat_agents.user_proxy,chat_agents.manager , contents))
                 chat_state.initiate_chat_task_created = True
             else:
                 if chat_state.input_future and not chat_state.input_future.done():
                         chat_state.input_future.set_result(contents)
+                else:
+                    print("There is currently no input being awaited.")
+        #---------------------------------------------------------------------------------------
+        #---------------------------------------------------------------------------------------
+        elif  chat_state.current_mode ==  mode_3 :
+            if not chat_state.initiate_chat_task_created: 
+                print("Chat mode: application_group")
+                chat_agents = content_group(mistral_7b_config)
+                register_agent_replies([chat_agents.user_proxy,chat_agents.content_creator,chat_agents.reviewer])
+                asyncio.create_task(delayed_initiate_chat(chat_agents.user_proxy, chat_agents.manager , contents))
+                chat_state.initiate_chat_task_created = True
+            else:
+                if chat_state.input_future and not chat_state.input_future.done():
+                    chat_state.input_future.set_result(contents)
                 else:
                     print("There is currently no input being awaited.")
         #---------------------------------------------------------------------------------------
@@ -137,8 +161,7 @@ async def callback(contents: str, user: str, instance: pn.chat.ChatInterface):
             if not chat_state.initiate_chat_task_created:
                 print("Chat mode: application_group")
                 chat_agents = application_group(mistral_7b_config)
-                register_agent_replies([chat_agents.user_proxy,chat_agents.user_proxy,
-                chat_agents.engineer,chat_agents.scientist,chat_agents.planner,chat_agents.executor ,chat_agents.critic])
+                register_agent_replies([chat_agents.user_proxy,chat_agents.coder,chat_agents.scientist,chat_agents.planner,chat_agents.executor ,chat_agents.critic])
                 asyncio.create_task(delayed_initiate_chat(chat_agents.user_proxy,chat_agents.manager , contents))
                 chat_state.initiate_chat_task_created = True
             else:
@@ -163,6 +186,48 @@ async def callback(contents: str, user: str, instance: pn.chat.ChatInterface):
                 else:
                     print("There is currently no input being awaited.")
 #---------------------------------------------------------------------------------------
+        elif file_path and "file" in contents:
+            chat_state.in_chat_mode = True
+            chat_state.current_mode = mode_3
+#---------------------------------------------------------------------------------------
+            path = f"C:/Project/LLM/{file_path}"
+            print("path:",path)
+            if not chat_state.initiate_chat_task_created: 
+                print("Chat mode: file")
+                assistant = RetrieveAssistantAgent(
+                    name="assistant",
+                    system_message="You are a helpful assistant.",
+                    llm_config=mistral_7b_config,
+                )
+                ragproxyagent = RetrieveUserProxyAgent(
+                    name="ragproxyagent",
+                    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+                    llm_config=mistral_7b_config,
+                    system_message="Assistant who has extra content retrieval power for solving difficult problems.",
+                    human_input_mode="NEVER",
+                    max_consecutive_auto_reply=3,   
+                    retrieve_config={
+                        "task": "code",
+                        "docs_path":  path ,
+                        # "docs_path": "https://raw.githubusercontent.com/microsoft/FLAML/main/website/docs/Examples/Integrate%20-%20Spark.md",
+                        "chunk_token_size": 1000,
+                        "custom_text_types": ["pdf"],
+                        "client": chromadb.PersistentClient(path="/tmp/chromadb"),
+                        "collection_name": "groupchat",
+                        # "embedding_model": "all-mpnet-base-v2",
+                        "get_or_create": True,
+                    },
+                    code_execution_config=False,  # set to False if you don't want to execute the code
+                )
+                assistant.reset()
+                register_agent_replies([assistant,ragproxyagent])
+                asyncio.create_task(delayed_initiate_chat(ragproxyagent,assistant, contents))
+                chat_state.initiate_chat_task_created = True
+            else:
+                if chat_state.input_future and not chat_state.input_future.done():
+                    chat_state.input_future.set_result(contents)
+                else:
+                    print("There is currently no input being awaited.")
         else:
             chat_state.in_chat_mode = True
             chat_state.current_mode = mode_normal
@@ -180,7 +245,57 @@ async def callback(contents: str, user: str, instance: pn.chat.ChatInterface):
                     print("There is currently no input being awaited.")
 #---------------------------------------------------------------------------------------
 
-chat_interface = pn.chat.ChatInterface(callback=callback)
+chat_interface = pn.chat.ChatInterface(
+    callback=callback,
+
+    show_button_name=False,
+    sizing_mode="stretch_both",
+    min_height=600,
+)
+
+# chat_interface.send("Ask your question about the document!!", user="System", respond=False)
+
+uploading = pn.indicators.LoadingSpinner(value=False, size=50, name='No document')
+file_input = pn.widgets.FileInput(name="PDF File", accept=".pdf")
+text_area = pn.widgets.TextAreaInput(name='File Info', sizing_mode='stretch_both', min_height=400)
 chat_interface.send("Send a message!", user="System", respond=False)
-chat_interface.servable()
+
+async def file_callback(*events):
+    global file_path  # Khai báo biến toàn cục
+    file_path = ""
+    for event in events:
+        if event.name == 'filename':
+            file_name = event.new
+        if event.name == 'value':
+            file_content = event.new
+       
+    uploading.value = True
+    uploading.name = 'Uploading'
+    file_path = file_name
+    with open(file_path, 'wb') as f:
+        f.write(file_content)
+    # print(file_path)
+   
+    text_area.value = str( file_path)
+    uploading.value = False
+    uploading.name = f"Document uploaded - {file_name}"
+       
+
+# Set up a callback on file input value changes
+file_input.param.watch(file_callback, ['value', 'filename'])
+
+title = '## Please upload your document for RAG'
+file_app = pn.Column(pn.pane.Markdown(title), file_input, uploading, text_area, sizing_mode='stretch_width', min_height=300)
+
+pn.template.FastListTemplate(
+    title="📚Chat bot agents",
+    header_background="#2F4F4F",
+    accent_base_color="#2F4F4F",
+    main=[
+        chat_interface
+    ],
+    sidebar=[file_app],
+    sidebar_width=400,
+).servable()
+
 
